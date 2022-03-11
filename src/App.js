0ac4324aee3c3c126c2435bed07399a2342dd5ac
Sorrawit <sorrawit456789@gmail.com>
2022-03-11 23:34:01 +0700

    add Todo.js path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import Todo from "./Todo";
 
 function App() {
-  return <div>
+  return (
+  <div>
     <h1>My Todos</h1>
-    <div className='card'>
-      <h2>Title</h2>
-      <div className='actions'>
-        <button className='btn'>Delete</button>
-      </div>
-    </div>
-  </div>;
+    <Todo />
+    <Todo />
+  </div>
+  );
 }
 
 export default App;
