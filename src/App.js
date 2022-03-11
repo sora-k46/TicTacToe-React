80ad2e25f0e702371daa5657ead1f78c0d7a0063
Sorrawit <sorrawit456789@gmail.com>
2022-03-11 23:48:49 +0700

    remove white space

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
   <div>
     <h1>My Todos</h1>
-    <Todo />
-    <Todo />
+    <Todo text='Learn React'/>
+    <Todo text='Master React'/>
+    <Todo text='Explore the full React course'/>
   </div>
   );
 }
