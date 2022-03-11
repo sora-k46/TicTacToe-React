59a13a32a4ba2d8452e9159d668e267d7de27680
Sorrawit <sorrawit456789@gmail.com>
2022-03-11 23:33:32 +0700

    delete div and h1

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,12 @@
 function Todo() {
-    return <div>
-    <h1>My Todos</h1>
+    return (
     <div className='card'>
       <h2>Title</h2>
       <div className='actions'>
         <button className='btn'>Delete</button>
       </div>
     </div>
-  </div>;
+    );
 }
 
 export default Todo;
