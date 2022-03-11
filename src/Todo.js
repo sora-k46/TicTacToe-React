74298c0703db9d0de7a69335560c7d77f82a0f72
Sorrawit <sorrawit456789@gmail.com>
2022-03-11 23:27:47 +0700

    move Todo component to individual file

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+function Todo() {
+    return <div>
+    <h1>My Todos</h1>
+    <div className='card'>
+      <h2>Title</h2>
+      <div className='actions'>
+        <button className='btn'>Delete</button>
+      </div>
+    </div>
+  </div>;
+}
+
+export default Todo;
