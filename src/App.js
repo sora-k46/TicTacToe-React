39d2765e5d46c5805f497e6fc26c95365b4c2c52
Sorrawit <sorrawit456789@gmail.com>
2022-03-14 23:03:21 +0700

    remove tutorial code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import Todo from "./components/Todo";
 
 function App() {
   return (
+    <div>Hello</div>
+  );
+}
+
+export default App;
+
+/* 
+    From tutorial
+    https://www.youtube.com/watch?v=Dorf8i6lCuk&list=PLyc4jh-YhPOkx9LPi08v3NrZx5BUD6FqR
     <div>
       <h1>My Todos</h1>
       <Todo text='Learn React' />
       <Todo text='Master React' />
       <Todo text='Explore the full React course' />
     </div>
-  );
-}
-
-export default App;
+ */
