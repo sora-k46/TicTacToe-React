5ce1d58282c2b007f7cabacbee3aaf21df8b80b7
Sorrawit <sorrawit456789@gmail.com>
2022-04-11 13:15:18 +0700

    remove style and component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import Navbar from "./components/Navbar";
-import Aside from "./components/Aside";
 
 function App() {
   return (
     <>
-      <Navbar />
-      {/* <div className='container'>
-        <Aside />
-        <main>
-          this is main blog.
-        </main>
-      </div> */}
+      <p>Hello</p>
     </>
   );
 }
 
 export default App;
-
-/* 
-    From tutorial
-    https://www.youtube.com/watch?v=Dorf8i6lCuk&list=PLyc4jh-YhPOkx9LPi08v3NrZx5BUD6FqR
-    <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React' />
-      <Todo text='Master React' />
-      <Todo text='Explore the full React course' />
-    </div>
- */
